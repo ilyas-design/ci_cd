7de62954dd0f99d17e04fb66f9f03e3b24c3323d
Ilyas <ilyas.lachgar@ecoles-epsi.net>
2025-11-05 10:22:38 +0100

    nothing to commit

--- lib/pages/countries_page.dart
+++ lib/pages/countries_page.dart
@@ -111,7 +111,7 @@ class _CountriesPageState extends State<CountriesPage> {
         color: Theme.of(context).colorScheme.surface,
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 10,
             offset: const Offset(0, 2),
           ),
